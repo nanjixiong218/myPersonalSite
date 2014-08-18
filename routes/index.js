35325d26bc43ba23459aad2242eabd74a0c9ac8f
xu <695540791@qq.com>
2014-08-18 22:32:13 +0800

    星空首页完成

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var router = express.Router();
 var index = require('../controller/home.js');
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.sendfile('views/myStarIndex.html');
+});
+router.get('/index', function(req, res) {
   res.render('index');
 });
 
