e73226ce25710fc24286b59b352609b1bd6984b8
xu <695540791@qq.com>
2014-08-19 17:07:27 +0800

    sendfile问题修改

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var router = express.Router();
 var index = require('../controller/home.js');
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.sendfile('views/static/myStarIndex.html');
+    //好奇怪，sendfile再养在服务器上找不到,本地却可以
+   //res.sendfile('views/static/myStarIndex.html');
+    res.render('myStarIndex',{layout:false});
 });
 router.get('/index', function(req, res) {
   res.render('index');
@@ -12,8 +14,9 @@ router.get('/index', function(req, res) {
 router.get('/about', function(req, res) {
   res.render('about');
 });
+/*同样，这个在服务器上也不行
 router.get('/todo', function(req, res) {
   res.sendfile('views/static/ng-todo.html');
 });
-
+*/
 module.exports = router;
