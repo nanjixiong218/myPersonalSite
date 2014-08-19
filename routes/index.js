5a548fee885d00bca178c2a3c7eed5c3bfaf026c
xu <695540791@qq.com>
2014-08-19 11:57:10 +0800

    加入todo

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,16 @@ var router = express.Router();
 var index = require('../controller/home.js');
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.sendfile('views/myStarIndex.html');
+  res.sendfile('views/static/myStarIndex.html');
 });
 router.get('/index', function(req, res) {
   res.render('index');
 });
 router.get('/about', function(req, res) {
   res.render('about');
+});
+router.get('/todo', function(req, res) {
+  res.sendfile('views/static/ng-todo.html');
 });
 
 module.exports = router;
